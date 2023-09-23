7aa95e759a80d0fe0813ea178d776f7de2663d0c
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-09-23 12:04:48 +0530

    Extracting Data From Requst Object Like HttmMethod,Query,Header,Body is Completed

--- index.js
+++ index.js
@@ -1,28 +1,92 @@
 var http = require('http');
 var url = require('url');
+var stringDecoder = require('string_decoder').StringDecoder;
 
-var server = http.createServer((req, res) => {
+var server = http.createServer(async(req, res) => {
 
-    var reqPath = getPath(req);
-    var httpMethod = getHttpMehtod(req);
-    if (!reqPath) {
-        console.log('then it is Hitting Without Route ' + httpMethod);
-    } else {
-        console.log(`host address ${reqPath} through ${httpMethod}`);
-    }
+    let requestObject = await getRequestData(req)
+    console.log(JSON.stringify(requestObject));
+
     // Generating Respsne...
     SendResponse(res);
 });
 
-function getPath(req) {
-    var parsed = url.parse(req.url)
-    var trimmedPath = parsed.pathname.replace(/^\/+|\/+$/g, '')
-    return trimmedPath;
+async function getRequestData(req) {
+    var Route = getPath(req);
+    var HttpMethod = getHttpMehtod(req);
+    var Params = getQueryString(req);
+    var Headers = getHeaders(req);
+    var Payload = await getPayloadPromise(req);
+    // var Payload=null;
+    // var task=  getPayloadPromise(req)
+    //         .then(x=> {
+    //             Payload=x;
+    //         });
+    //  console.log(task)
+    function getHeaders(req) {
+        return req.headers
+    }
+
+    function getHttpMehtod(req) {
+        return req.method.toLowerCase();
+    }
+
+    function getQueryString(req) {
+        return url.parse(req.url).query
+    }
+
+    function getPath(req) {
+        var parsed = url.parse(req.url)
+        var trimmedPath = parsed.pathname.replace(/^\/+|\/+$/g, '')
+        return trimmedPath;
+    }
+
+    function getPayloadPromise(req) {
+        return new Promise((resolve, reject) => {
+
+            const DECODEUTF8 = new stringDecoder('utf-8');
+            var buffer = '';
+            var payload = null;
+            req.on('data', function(data) {
+                buffer += DECODEUTF8.write(data);
+            });
+            req.on('end', () => {
+                buffer += DECODEUTF8.end();
+                payload = JSON.parse(buffer);
+                resolve(payload);
+            })
+
+        })
+    }
+
+    function getPayload(req) {
+        const DECODEUTF8 = new stringDecoder('utf-8');
+        var buffer = '';
+        var payload = null;
+        req.on('data', function(data) {
+
+            buffer += DECODEUTF8.write(data);
+        });
+        req.on('end', () => {
+            buffer += DECODEUTF8.end();
+            payload = JSON.parse(buffer);
+            console.log('Payload...')
+            console.log(payload);
+        })
+        return payload;
+    }
+
+
+    return {
+        Route,
+        HttpMethod,
+        Params,
+        Headers,
+        Payload
+    }
 }
 
-function getHttpMehtod(req) {
-    return req.method.toLowerCase();
-}
+
 
 function SendResponse(res) {
     // Set response headers (e.g., content type)
@@ -34,6 +98,8 @@ function SendResponse(res) {
     // End the response (send it to the client)
     res.end();
 }
+
+
 server.listen(3000, () => {
     console.log('This Server is Listnening in the Port 3000');
 });
